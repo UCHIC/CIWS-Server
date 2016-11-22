6c949ef3690c6509b0b0ab59a30f6c0a3741cc57
Karun Joseph <karun.joseph@aggiemail.usu.edu>
2016-11-22 07:39:17 -0700

    changes to db schema

--- loaddata.sql
+++ loaddata.sql
@@ -2,9 +2,13 @@
 USE waterusedb;
 
 -- Insert data into sites
-INSERT INTO `sites` (`site_id`,`site_code`,`site_name`,`site_lat`, `site_long`, `site_type`, `site_comments`)              
-VALUES (1, 'LLC_A', 'Living and Learning Center', 35, 111, 'hot', 'None');
+INSERT INTO `sites` (`site_id`,`site_code`,`site_name`,`site_lat`, `site_long`, `site_type`, `site_comments`, `site_dst`)              
+VALUES (1, 'LLC_A', 'Living and Learning Center', 35, 111, 'hot', 'None', 0);
 
 -- Insert data into variable
 INSERT INTO `variable` (`variable_id`, `variable_code`, `variable_name`, `variable_unit`, `variable_method`)
 VALUES (1, 'inst_water_vol', 'water usage', 'gal/min', '4-20mA reading using Raspberry Pi');
+
+-- Insert data into timeseries
+INSERT INTO `timeseries` (`timeseries_id`, `timeseries_utc_offset`, `timeseries_begin_datetime_utc`, `timeseries_end_datetime_utc`, `variable_variable_id`, `sites_site_id`)
+VALUES (1, -7, '2016-11-10 00:00:00', '2016-11-10 00:05:00', 1, 1);
