2fd608b11fb72119785ba81aeadee0dfa69e4ed5
Ubuntu <karun.joseph@aggiemail.usu.edu>
2016-12-01 14:29:46 +0000

    changed datetime_utc to datetime_local in dbhandler

--- dbhandler.py
+++ dbhandler.py
@@ -20,15 +20,15 @@ def writewaterusedb(datavalues, timeseries_id):
             
             # Create new datavalue records 
             datavalue_value = []
-            datavalue_datetime_utc = []
+            datavalue_datetime_local = []
             for datavalue in datavalues:
                 datavalue_value.append(datavalue['datavalue_value'])
-                datavalue_datetime_utc.append(datavalue['datavalue_datetime_utc'])
-            print datavalue_value, datavalue_datetime_utc 
+                datavalue_datetime_local.append(datavalue['datavalue_datetime_local'])
+            print datavalue_value, datavalue_datetime_local 
  
             #Assuming only 2 datavalues per object
-            sql = "INSERT INTO `datavalue` (`datavalue_value`, `datavalue_datetime_utc`, `timeseries_timeseries_id`) VALUES (%s, %s, %s), (%s, %s, %s)"
-            cursor.execute(sql, (datavalue_value[0], datavalue_datetime_utc[0], timeseries_id, datavalue_value[1], datavalue_datetime_utc[1], timeseries_id))
+            sql = "INSERT INTO `datavalue` (`datavalue_value`, `datavalue_datetime_local`, `timeseries_timeseries_id`) VALUES (%s, %s, %s), (%s, %s, %s)"
+            cursor.execute(sql, (datavalue_value[0], datavalue_datetime_local[0], timeseries_id, datavalue_value[1], datavalue_datetime_local[1], timeseries_id))
 
             # connection is not autocommit by default. So you must commit to save
             # your changes.
