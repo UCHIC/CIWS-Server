fbbdc7b2264804e5074c36d4cc8041f393a51124
Juan <juan.caraballo17@gmail.com>
2020-11-19 23:34:00 -0400

    Added logging to the web service.

--- src/ciws_ci/data_posting_service/web_service.py
+++ src/ciws_ci/data_posting_service/web_service.py
@@ -1,18 +1,23 @@
+import logging
 import os
 import sys
+
 import bottle
 
 from typing import Dict, List, Any
 
-from common import get_app_config
+from bottle import Request
+
+from common import get_app_config, create_logger
 from data_posting_service import auth
 
 
-def get_file_save_directory() -> str:
-    """ Gets the directory where the files will be copied to """
-
-    default_directory: str = 'data'
-    return config.get('source_directory', default_directory)
+def log_to_logger(callback):
+    def wrapper(*args, **kwargs):
+        request: Request = bottle.request
+        logger.info(f'{bottle.request.remote_addr} - {request.method} {request.path} : {bottle.response.status}')
+        return callback(*args, **kwargs)
+    return wrapper
 
 
 # @bottle.get('/data-api')
@@ -22,15 +27,18 @@ def get_file_save_directory() -> str:
 @bottle.post('/auth')
 def authorize_upload() -> str:
     """ Generates an upload token for a client """
-    return auth.generate_token()
+    token: str = auth.generate_token()
+    logger.info(f'upload token {token} generated for files {", ".join(bottle.request.forms.getall("filenames"))}')
+    return token
 
 
 @bottle.post('/data-api')
 def data_api_upload() -> Dict[str, str]:
     """ Saves csv files in the source folder for later processing """
     auth.authorize()
+    logger.info('data upload authorized!')
 
-    save_path: str = get_file_save_directory()
+    save_path: str = config.get('source_directory', 'data')
     uploaded_file: bottle.FileUpload
     messages: Dict[str, str] = {}
     name: str
@@ -39,20 +47,26 @@ def data_api_upload() -> Dict[str, str]:
     files: List[bottle.FileUpload] = bottle.request.files.getall('data_file[]')
 
     if len(files) == 0:
+        logger.warning('no files requested for upload.')
         messages['no-file'] = 'No files to be uploaded.'
         return messages
 
     for uploaded_file in files:
         name, ext = os.path.splitext(uploaded_file.filename)
         if ext.lower() not in ('.csv',):
+            logger.warning(f'file {name} not uploaded because it has an unsupported file type: {ext}')
             messages[uploaded_file.filename] = f'File {uploaded_file.filename} has an unsupported file type: {ext}'
             continue
         try:
+            logger.info(f'uploading file {uploaded_file.filename} to {save_path}')
             uploaded_file.save(save_path)
+            logger.info(f'file {uploaded_file.filename} uploaded successfully!')
             messages[uploaded_file.filename] = f'File {uploaded_file.filename} uploaded successfully'
         except IOError as ioe:
+            logger.error(f'file {uploaded_file.filename} could not be uploaded: {ioe}')
             messages[uploaded_file.filename] = f'File {uploaded_file.filename} not uploaded: {ioe}'
 
+    logger.info('all files processed successfully')
     return messages
 
 
@@ -61,7 +75,10 @@ try:
 except IOError as ioe:
     sys.exit("Settings file not found!")
 
+logger: logging.Logger = create_logger('data_poster', config.get('log_directory'))
+logger.info('logger started!')
 application = bottle.default_app()
+application.install(log_to_logger)
 
 
 if __name__ == '__main__':
